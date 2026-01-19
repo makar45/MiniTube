a7a048530746daa029aee190260dfb98670c673d
makar45 <calkovm862@gmail.com>
2026-01-19 20:19:28 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,7 @@
-// supabaseClient.js
 const { createClient } = require("@supabase/supabase-js");
 
-const supabaseUrl = "https://eognlslnsxbfcglyrlma.supabase.co"; // твой Project URL
-const supabaseKey = "sb_publishable_H69w8Hy7dlf6xDxayOMcww_4c1QGWbL"; // твой Publishable API Key
+const supabaseUrl = "https://eognlslnsxbfcglyrlma.supabase.co";
+const supabaseKey = "sb_publishable_H69w8Hy7dlf6xDxayOMcww_4c1QGWbL";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
