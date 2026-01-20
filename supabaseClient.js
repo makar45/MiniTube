349961a8dc0072ce509e540f0cd2d4c1919e4ec4
makar45 <calkovm862@gmail.com>
2026-01-20 17:47:25 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,7 @@
 const { createClient } = require("@supabase/supabase-js");
 
-const supabaseUrl = "https://eognlslnsxbfcglyrlma.supabase.co"; // твой Supabase URL
-const supabaseKey = "sb_publishable_H69w8Hy7dlf6xDxayOMcww_4c1QGWbL"; // твой ключ
-
+const supabaseUrl = "https://eognlslnsxbfcglyrlma.supabase.co"; // твой URL Supabase
+const supabaseKey = "sb_publishable_H69w8Hy7dlf6xDxayOMcww_4c1QGWbL"; // твой ANON key
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = supabase;
